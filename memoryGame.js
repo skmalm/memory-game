1cf35e78b487b5b533ab2c4f7f75807139a92456
Sebastian-Malm <skmalm@gmail.com>
2018-07-11 00:03:55 -0400

    feat: add card color array and intial loop to add colors to cards

--- memoryGame.js
+++ memoryGame.js
@@ -1,1 +1,22 @@
-alert("connected");
+const cards = document.querySelectorAll(".card");
+let cardColors = [
+  "brown",
+  "brown",
+  "dimGray",
+  "dimGray",
+  "green",
+  "green",
+  "maroon",
+  "maroon",
+  "navy",
+  "navy",
+  "olive",
+  "olive",
+  "purple",
+  "purple",
+  "teal",
+  "teal"]
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].style.backgroundColor = cardColors[i];
+}
